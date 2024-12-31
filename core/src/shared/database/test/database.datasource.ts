1eeef91f318bcefadda979eb7876f2535d7b3d05
Oleh Strokan <oleg14ua71@gmail.com>
2025-01-01 00:48:13 +0100

    update db connection, add metrics interceptors

--- core/src/shared/database/test/database.datasource.ts
+++ core/src/shared/database/test/database.datasource.ts
@@ -8,7 +8,7 @@ config();
 export default new DataSource({
   type: 'postgres',
   host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT, 10) || 6434,
+  port: Number(process.env.DB_PORT) || 6434,
   username: process.env.DB_USER || 'stroka01',
   password: process.env.DB_PASSWORD || 'test',
   database: process.env.DB_NAME || 'test_exchange_db',
