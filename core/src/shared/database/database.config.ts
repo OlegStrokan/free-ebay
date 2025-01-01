d9961e3f0cfd971547fdd7e642c8a7ce2549ff82
Oleh Strokan <oleg14ua71@gmail.com>
2025-01-01 06:06:12 +0100

    add category to db config

--- core/src/shared/database/database.config.ts
+++ core/src/shared/database/database.config.ts
@@ -1,4 +1,5 @@
 import { registerAs } from '@nestjs/config';
+import { CategoryDb } from 'src/catalog/infrastructure/entity/category';
 import { ProductDb } from 'src/product/infrastructure/entity/product.entity';
 import { UserDb } from 'src/user/infrastructure/entity/user.entity';
 
@@ -9,7 +10,7 @@ export const DbConfig = registerAs('exchange', () => ({
   username: process.env.DB_USER || 'stroka01',
   password: process.env.DB_PASSWORD || 'admin',
   database: process.env.DB_NAME || 'exchange',
-  entities: [ProductDb, UserDb],
+  entities: [ProductDb, UserDb, CategoryDb],
   logging: process.env.NODE_ENV === 'development',
   migrations: [`${__dirname}/migrations/*{.ts,.js}`],
   migrationsTableName: 'migrations',
--- core/src/shared/database/database.datasource.ts
+++ core/src/shared/database/database.datasource.ts
@@ -2,6 +2,7 @@ import { DataSource } from 'typeorm';
 import { config } from 'dotenv';
 import { ProductDb } from 'src/product/infrastructure/entity/product.entity';
 import { UserDb } from 'src/user/infrastructure/entity/user.entity';
+import { CategoryDb } from 'src/catalog/infrastructure/entity/category';
 
 config();
 
@@ -12,7 +13,7 @@ export default new DataSource({
   username: process.env.DB_USER || 'stroka01',
   password: process.env.DB_PASSWORD || 'admin',
   database: process.env.DB_NAME || 'exchange',
-  entities: [ProductDb, UserDb],
+  entities: [ProductDb, UserDb, CategoryDb],
   logging: process.env.NODE_ENV === 'development',
   migrations: [`${__dirname}/migrations/*{.ts,.js}`],
   migrationsTableName: 'migrations',
