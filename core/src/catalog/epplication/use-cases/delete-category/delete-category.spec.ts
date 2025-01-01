7b59454742d37b66fcbdfe198eb235d8fc4c059b
Oleh Strokan <oleg14ua71@gmail.com>
2025-01-01 06:31:23 +0100

    add delete category tests

---
+++ core/src/catalog/epplication/use-cases/delete-category/delete-category.spec.ts
@@ -0,0 +1,45 @@
+import { TestingModule } from '@nestjs/testing';
+import { clearRepos } from 'src/shared/testing/clear-repos';
+import { createTestingModule } from 'src/shared/testing/test.module';
+import { DeleteCategoryUseCase } from './delete-category.use-case';
+import { ICategoryMockService } from 'src/catalog/core/category/entity/mocks/category-mock.interface';
+import { CategoryMockService } from 'src/catalog/core/category/entity/mocks/category-mock.service';
+import { IDeleteCategoryUseCase } from './delete-category.interface';
+import { CategoryNotFoundException } from 'src/catalog/core/category/entity/exceptions/category-not-found.exception';
+
+describe('GeleteCategoryUseCaseTest', () => {
+  let deleteCategoryUseCase: IDeleteCategoryUseCase;
+  let categoryMockService: ICategoryMockService;
+  let module: TestingModule;
+
+  beforeAll(async () => {
+    module = await createTestingModule();
+
+    deleteCategoryUseCase = module.get<IDeleteCategoryUseCase>(
+      DeleteCategoryUseCase,
+    );
+    categoryMockService = module.get<ICategoryMockService>(CategoryMockService);
+  });
+
+  afterAll(async () => {
+    await module.close();
+  });
+
+  beforeEach(async () => {
+    await clearRepos(module);
+  });
+
+  it('should succesfully delete category', async () => {
+    const product = await categoryMockService.createOne();
+
+    const retrievedProduct = await deleteCategoryUseCase.execute(product.id);
+
+    expect(retrievedProduct).not.toBeDefined();
+  });
+
+  it('should succesfully delete category', async () => {
+    await expect(
+      deleteCategoryUseCase.execute('not_existing_id'),
+    ).rejects.toThrow(CategoryNotFoundException);
+  });
+});
