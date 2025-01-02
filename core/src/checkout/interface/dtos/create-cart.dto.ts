4c457a683d3889e44f89006fcd4b127402b2e46e
Oleh Strokan <oleg14ua71@gmail.com>
2025-01-02 13:30:21 +0100

    add cart repository

--- core/src/checkout/interface/dtos/create-cart.dto.ts
+++ core/src/checkout/interface/dtos/create-cart.dto.ts
@@ -1,21 +1,8 @@
-import {
-  IsNotEmpty,
-  IsUUID,
-  ValidateNested,
-  IsOptional,
-  IsArray,
-} from 'class-validator';
-import { Type } from 'class-transformer';
-import { AddToCartDto } from './add-to-cart.dto';
+import { IsUUID } from 'class-validator';
+import { IsNotEmpty } from 'class-validator';
 
 export class CreateCartDto {
   @IsUUID()
   @IsNotEmpty()
   userId!: string;
-
-  @IsOptional()
-  @IsArray()
-  @ValidateNested({ each: true })
-  @Type(() => AddToCartDto)
-  items?: AddToCartDto[];
 }
