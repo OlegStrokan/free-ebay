08f04459d97a69f6d5e1963ee605e1356c9dae5b
Oleh Strokan <oleg14ua71@gmail.com>
2025-01-02 21:00:27 +0100

    add create cart use-case

--- core/src/checkout/epplication/use-cases/create-cart/create-cart.interface.ts
+++ core/src/checkout/epplication/use-cases/create-cart/create-cart.interface.ts
@@ -1,4 +1,4 @@
-import { Cart } from 'src/checkout/core/entity/card';
+import { Cart } from 'src/checkout/core/entity/cart/cart';
 import { CreateCartDto } from 'src/checkout/interface/dtos/create-cart.dto';
 import { IUseCase } from 'src/shared/types/use-case.interface';
 
---
+++ core/src/checkout/epplication/use-cases/create-cart/create-cart.use-case.ts
@@ -0,0 +1,34 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { ICreateCartUseCase } from './create-cart.interface';
+import { ICartRepository } from 'src/checkout/core/repository/cart.repository';
+import { CART_REPOSITORY } from '../../injection-tokens/repository.token';
+import { Cart } from 'src/checkout/core/entity/cart/cart';
+import { CreateCartDto } from 'src/checkout/interface/dtos/create-cart.dto';
+import { CartAlreadyExists } from 'src/checkout/core/exceptions/cart/cart-already-exist';
+import {
+  IUserRepository,
+  USER_REPOSITORY,
+} from 'src/user/core/repository/user.repository';
+import { UserNotFoundException } from 'src/user/core/exceptions/user-not-found.exception';
+
+@Injectable()
+export class CreateCartUseCase implements ICreateCartUseCase {
+  constructor(
+    @Inject(CART_REPOSITORY)
+    private readonly cartRepository: ICartRepository,
+    @Inject(USER_REPOSITORY)
+    private readonly userRepository: IUserRepository,
+  ) {}
+  async execute(dto: CreateCartDto): Promise<Cart> {
+    const user = await this.userRepository.findById(dto.userId);
+    if (!user) {
+      throw new UserNotFoundException('id', dto.userId);
+    }
+    const existingCart = await this.cartRepository.getCartByUserId(dto.userId);
+    if (existingCart) {
+      throw new CartAlreadyExists(dto.userId);
+    }
+    const cart = Cart.create(dto.userId);
+    return await this.cartRepository.saveCart(cart);
+  }
+}
