36a428ece26defbf019a7c1653fd64e5b2ce26b8
Oleh Strokan <oleg14ua71@gmail.com>
2025-01-02 13:29:19 +0100

    update user entity

--- core/src/user/infrastructure/entity/user.entity.ts
+++ core/src/user/infrastructure/entity/user.entity.ts
@@ -1,15 +1,7 @@
-import { CartDb } from 'src/checkout/infrastructure/entity/card.entity';
 import { OrderDb } from 'src/checkout/infrastructure/entity/order.entity';
 import { PaymentDb } from 'src/checkout/infrastructure/entity/payment.entity';
 import { BaseEntity } from 'src/shared/types/base-entity/base.entity';
-import {
-  Entity,
-  Column,
-  PrimaryColumn,
-  JoinColumn,
-  OneToMany,
-  OneToOne,
-} from 'typeorm';
+import { Entity, Column, PrimaryColumn, OneToMany } from 'typeorm';
 
 @Entity('users')
 export class UserDb extends BaseEntity {
@@ -21,10 +13,6 @@ export class UserDb extends BaseEntity {
 
   @Column()
   password!: string;
-
-  @OneToOne(() => CartDb, (cart) => cart.user, { onDelete: 'CASCADE' })
-  @JoinColumn({ name: 'cart_id' })
-  cart!: CartDb;
 
   @OneToMany(() => OrderDb, (order) => order.user)
   orders!: OrderDb[];
