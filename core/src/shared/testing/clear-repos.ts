8a439f4a90fcec66caac83101a486ff1438cbcd2
Oleh Strokan <oleg14ua71@gmail.com>
2025-01-10 14:22:48 +0100

    update clear-repos helper method

--- core/src/shared/testing/clear-repos.ts
+++ core/src/shared/testing/clear-repos.ts
@@ -1,13 +1,8 @@
 import { TestingModule } from '@nestjs/testing';
 import { IProductRepository } from 'src/product/core/product/repository/product.repository';
-import { ProductRepository } from 'src/product/infrastructure/repository/product.repository';
 import { IClearableRepository } from '../types/clearable';
 import { IUserRepository } from 'src/user/core/repository/user.repository';
-import { UserRepository } from 'src/user/infrastructure/repository/user.repository';
-import {
-  CATEGORY_REPOSITORY,
-  ICategoryRepository,
-} from 'src/catalog/core/category/repository/category.repository';
+import { ICategoryRepository } from 'src/catalog/core/category/repository/category.repository';
 import { ICartRepository } from 'src/checkout/core/repository/cart.repository';
 import {
   CART_REPOSITORY,
@@ -19,10 +14,12 @@ import { IOrderRepository } from 'src/checkout/core/repository/order.repository'
 import { IPaymentRepository } from 'src/checkout/core/repository/payment.repository';
 import { IShipmentRepository } from 'src/checkout/core/repository/shipment.repository';
 import { USER_REPOSITORY } from 'src/user/epplication/injection-tokens/repository.token';
+import { PRODUCT_REPOSITORY } from 'src/product/epplication/injection-tokens/repository.token';
+import { CATEGORY_REPOSITORY } from 'src/catalog/epplication/injection-tokens/repository.token';
 
 export const clearRepos = async (module: TestingModule) => {
   const repositories = [
-    module.get<IProductRepository>(ProductRepository),
+    module.get<IProductRepository>(PRODUCT_REPOSITORY),
     module.get<IUserRepository>(USER_REPOSITORY),
     module.get<ICategoryRepository>(CATEGORY_REPOSITORY),
     module.get<ICartRepository>(CART_REPOSITORY),
