a5f6fe407301b0f571e9be015d9e4801f98bc774
Oleh Strokan <oleg14ua71@gmail.com>
2025-01-14 08:40:37 +0100

    add entities to payment-service

--- payment-service/payment-service/Entities/Money.cs
+++ payment-service/payment-service/Entities/Money.cs
@@ -2,5 +2,19 @@ namespace payment_service.Entities;
 
 public class Money
 {
-    
+    public int Amount { get; set; }
+    public string Currency { get; set; }
+    public int Fraction { get; set; }
+
+    public Money(int amount, string currency, int fraction)
+    {
+        Amount = amount;
+        Currency = currency;
+        Fraction = fraction;
+    }
+
+    public string Format()
+    {
+        return $"{Amount} {Currency} {Fraction}";
+    }
 }
--- payment-service/payment-service/Entities/Payment.cs
+++ payment-service/payment-service/Entities/Payment.cs
@@ -1,6 +1,11 @@
-namespace payment_service.entities;
+using payment_service.Enums;
+
+namespace payment_service.Entities;
 
 public class Payment
 {
+    public string OrderId { get; set; }
+    public Money Amount { get; set; }
+    public PaymentMethod PaymentMethod { get; set; }
     
 }
