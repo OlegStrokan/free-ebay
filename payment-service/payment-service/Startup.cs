b537565d4e7ecb48c62e3ed3c02fbf67c3abfbbb
Oleh Strokan <oleg14ua71@gmail.com>
2025-01-14 08:41:22 +0100

    add startup function

--- payment-service/payment-service/Startup.cs
+++ payment-service/payment-service/Startup.cs
@@ -1,6 +1,26 @@
-namespace payment_service;
+
+using payment_service.Services;
 
 public class Startup
 {
-    
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddHostedService<PaymentService>(); 
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseRouting();
+        app.UseAuthorization();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
 }
