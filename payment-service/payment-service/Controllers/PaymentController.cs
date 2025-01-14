8b76246bbf0719ec6e28befd49e4a16b0f5f2d15
Oleh Strokan <oleg14ua71@gmail.com>
2025-01-14 08:41:04 +0100

    add  service + controller to payment-service

--- payment-service/payment-service/Controllers/PaymentController.cs
+++ payment-service/payment-service/Controllers/PaymentController.cs
@@ -1,6 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using payment_service.Entities;
+using payment_service.Services;
+
 namespace payment_service.Controllers;
 
-public class PaymentContoller
-{
-    
-}
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PaymentController : ControllerBase
+    {
+        private readonly PaymentService _paymentService;
+
+        public PaymentController(PaymentService paymentService)
+        {
+            _paymentService = paymentService;
+        }
+
+        [HttpPost]
+        public IActionResult ProcessPayment([FromBody] Payment payment)
+        {
+            if (payment == null)
+            {
+                return BadRequest(new { Message = "Payment data is required." });
+            }
+
+            var result = _paymentService.ProcessMessage(payment);
+
+            return Ok(result);
+        }
+    }
