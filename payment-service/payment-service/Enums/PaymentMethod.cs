574f0bd1a53fe5158c2b6d9d5a9caec797258a70
Oleh Strokan <oleg14ua71@gmail.com>
2025-01-14 08:41:33 +0100

    add paymentMethod enum to payment-service

--- payment-service/payment-service/Enums/PaymentMethod.cs
+++ payment-service/payment-service/Enums/PaymentMethod.cs
@@ -1,6 +1,21 @@
+using System.ComponentModel;
+
 namespace payment_service.Enums;
 
-public enum PaymentMethod
-{
-    
-}
+    public enum PaymentMethod
+    {
+        [Description("CreditCard")]
+        CreditCard,
+        [Description("PayPal")]
+        PayPal,
+        [Description("BankTransfer")]
+        BankTransfer,
+        [Description("CashOnDelivery")]
+        CashOnDelivery,
+        [Description("ApplePay")]
+        ApplePay,
+        [Description("GooglePay")]
+        GooglePay,
+        [Description("Cryptocurrency")]
+        Cryptocurrency
+    }
