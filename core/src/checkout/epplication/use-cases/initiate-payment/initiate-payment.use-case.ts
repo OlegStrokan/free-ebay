5a3befef8fc4296301ac47a4213becb8cd937913
Oleh Strokan <oleg14ua71@gmail.com>
2025-01-18 16:49:47 +0100

    create initiate payment use-case

---
+++ core/src/checkout/epplication/use-cases/initiate-payment/initiate-payment.use-case.ts
@@ -0,0 +1,103 @@
+import { HttpService } from '@nestjs/axios';
+import { Injectable, Inject } from '@nestjs/common';
+import { firstValueFrom } from 'rxjs';
+import {
+  Payment,
+  PaymentMethod,
+} from 'src/checkout/core/entity/payment/payment';
+import { Shipment } from 'src/checkout/core/entity/shipment/shipment';
+import { PaymentFailedException } from 'src/checkout/core/exceptions/payment/payment-failed.exception';
+import { IPaymentRepository } from 'src/checkout/core/repository/payment.repository';
+import { IShipmentRepository } from 'src/checkout/core/repository/shipment.repository';
+import { Money } from 'src/shared/types/money';
+import {
+  SHIPMENT_REPOSITORY,
+  PAYMENT_REPOSITORY,
+} from '../../injection-tokens/repository.token';
+
+@Injectable()
+export class InitiatePaymentUseCase {
+  constructor(
+    @Inject(SHIPMENT_REPOSITORY)
+    private readonly shipmentRepository: IShipmentRepository,
+    @Inject(PAYMENT_REPOSITORY)
+    private readonly paymentRepository: IPaymentRepository,
+    private readonly httpService: HttpService,
+  ) {}
+
+  async execute(dto: InitiatePaymentDto): Promise<PaymentResult> {
+    const shipment = await this.createShipment(
+      dto.orderId,
+      dto.shippingAddress,
+    );
+
+    const payment = await this.createPayment(
+      dto.orderId,
+      dto.paymentMethod,
+      dto.amount,
+    );
+
+    if (dto.paymentMethod !== PaymentMethod.CashOnDelivery) {
+      const response = await firstValueFrom(
+        await this.processPaymentInfo(payment),
+      );
+
+      if (response.status !== 200) {
+        throw new PaymentFailedException(dto.orderId);
+      }
+    }
+
+    return {
+      shipment,
+      payment,
+    };
+  }
+
+  private async createShipment(
+    orderId: string,
+    shippingAddress: string,
+  ): Promise<Shipment> {
+    const shipment = Shipment.create(orderId, shippingAddress);
+    await this.shipmentRepository.save(shipment);
+    return shipment;
+  }
+
+  private async createPayment(
+    orderId: string,
+    paymentMethod: PaymentMethod,
+    amount: Money,
+  ): Promise<Payment> {
+    const payment = Payment.create({ amount, paymentMethod, orderId });
+    await this.paymentRepository.save(payment);
+    return payment;
+  }
+
+  private async processPaymentInfo(payment: Payment) {
+    const paymentInfo = {
+      orderId: payment.orderId,
+      amount: {
+        amount: payment.amount.getAmount(),
+        fraction: payment.amount.getFraction(),
+        currency: payment.amount.getCurrency(),
+      },
+      paymentMethod: payment.paymentMethod,
+    };
+
+    return this.httpService.post(
+      'http://localhost:5012/api/Payment/ProcessPayment',
+      paymentInfo,
+    );
+  }
+}
+
+interface InitiatePaymentDto {
+  orderId: string;
+  paymentMethod: PaymentMethod;
+  amount: Money;
+  shippingAddress: string;
+}
+
+interface PaymentResult {
+  shipment: Shipment;
+  payment: Payment;
+}
