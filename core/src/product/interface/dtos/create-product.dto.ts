34790f60f40c3b0555a4160f9afaccf5dbea9e3c
Oleh Strokan <oleg14ua71@gmail.com>
2025-01-21 22:48:16 +0100

    add swagger to create product dto

--- core/src/product/interface/dtos/create-product.dto.ts
+++ core/src/product/interface/dtos/create-product.dto.ts
@@ -1,24 +1,47 @@
 import { IsString, IsNotEmpty, IsOptional, IsEnum } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 import { ProductStatus } from 'src/product/core/product/entity/product-status';
 import { Money } from 'src/shared/types/money';
 
 export class CreateProductDto {
+  @ApiProperty({
+    description: 'SKU of the product',
+    example: 'SKU12345',
+  })
   @IsString()
   @IsNotEmpty()
   sku!: string;
 
+  @ApiProperty({
+    description: 'Name of the product',
+    example: 'Wireless Mouse',
+  })
   @IsString()
   @IsNotEmpty()
   name!: string;
 
+  @ApiProperty({
+    description: 'Description of the product',
+    example: 'A high-quality wireless mouse with ergonomic design.',
+  })
   @IsString()
   @IsNotEmpty()
   description!: string;
 
+  @ApiProperty({
+    description: 'Status of the product',
+    enum: ProductStatus,
+    example: ProductStatus.Available,
+    required: false,
+  })
   @IsEnum(ProductStatus)
   @IsOptional()
   status?: ProductStatus = ProductStatus.Available;
 
+  @ApiProperty({
+    description: 'Price of the product',
+    example: { currency: 'USD', amount: 49.99 },
+  })
   @IsNotEmpty()
   price!: Money;
 }
