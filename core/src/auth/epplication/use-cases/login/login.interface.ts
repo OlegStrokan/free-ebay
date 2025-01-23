3a9a77fd1fc90c6a3e29c43e2a115cb03d71b4da
Oleh Strokan <oleg14ua71@gmail.com>
2025-01-23 14:59:58 +0100

    update login use-case

--- core/src/auth/epplication/use-cases/login/login.interface.ts
+++ core/src/auth/epplication/use-cases/login/login.interface.ts
@@ -1,4 +1,11 @@
-import { LoginDto } from 'src/auth/interface/dtos/login.dto';
+import { LoginRequestDto } from 'src/auth/interface/dtos/login-request.dto';
 import { IUseCase } from 'src/shared/types/use-case.interface';
+import { User } from 'src/user/core/entity/user';
 
-export type ILoginUseCase = IUseCase<LoginDto, any>;
+export type LoginResponseType = {
+  user: User;
+  accessToken: string;
+  refreshToken: string;
+};
+
+export type ILoginUseCase = IUseCase<LoginRequestDto, LoginResponseType>;
--- core/src/auth/epplication/use-cases/login/login.use-case.ts
+++ core/src/auth/epplication/use-cases/login/login.use-case.ts
@@ -1,20 +1,22 @@
-import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
-import { LoginDto } from 'src/auth/interface/dtos/login.dto';
+import { LoginRequestDto } from 'src/auth/interface/dtos/login-request.dto';
 import { TokenService } from '../../service/token.service';
 import { IUserRepository } from 'src/user/core/repository/user.repository';
 import { User } from 'src/user/core/entity/user';
 import { USER_REPOSITORY } from 'src/user/epplication/injection-tokens/repository.token';
+import { ILoginUseCase } from './login.interface';
+import { InvalidLoginCredentialsException } from 'src/auth/core/exceptions/invalid-login-credentials.exception';
 
 @Injectable()
-export class LoginUseCase {
+export class LoginUseCase implements ILoginUseCase {
   constructor(
     @Inject(USER_REPOSITORY)
     private readonly userRepository: IUserRepository,
     private readonly tokenService: TokenService,
   ) {}
 
-  async execute(dto: LoginDto) {
+  async execute(dto: LoginRequestDto) {
     const user = await this.validateUser(dto);
     const accessToken = this.tokenService.createAccessToken(user.data);
     const refreshToken = this.tokenService.createAccessToken(user.data);
@@ -22,18 +24,18 @@ export class LoginUseCase {
     return { user, accessToken, refreshToken };
   }
 
-  private async validateUser(dto: LoginDto): Promise<User> {
+  private async validateUser(dto: LoginRequestDto): Promise<User> {
     const alreadyExistingUser = await this.userRepository.findByEmail(
       dto.email,
     );
-    if (!alreadyExistingUser) throw new UnauthorizedException('Fuck you');
+    if (!alreadyExistingUser) throw new InvalidLoginCredentialsException();
 
     const isPasswordValid = await bcrypt.compare(
       dto.password,
       alreadyExistingUser.data.password,
     );
     if (!isPasswordValid) {
-      throw new UnauthorizedException('Fuck you again');
+      throw new InvalidLoginCredentialsException();
     }
     return alreadyExistingUser;
   }
