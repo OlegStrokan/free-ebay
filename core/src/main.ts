ebca11cb311bdb6aa3b553ce5e0a5b808e6dae6b
Oleh Strokan <oleg14ua71@gmail.com>
2025-01-23 14:59:26 +0100

    add swagger config to main.ts

--- core/src/main.ts
+++ core/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './shared/errors/http.exception.filter';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,16 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('ebay example')
+    .setDescription('The ebay API description')
+    .setVersion('1.0')
+    .addTag('ebay')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(3000);
 }
