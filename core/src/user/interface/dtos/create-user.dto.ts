cbc5e7e51d39bff09c35723707be68f2525ba294
Oleh Strokan <oleg14ua71@gmail.com>
2025-01-23 15:01:03 +0100

    update user dtos

--- core/src/user/interface/dtos/create-user.dto.ts
+++ core/src/user/interface/dtos/create-user.dto.ts
@@ -1,9 +1,18 @@
 import { IsEmail, IsString, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
+  @ApiProperty({
+    description: 'User email address',
+    example: 'user@example.com',
+  })
   @IsEmail()
   email!: string;
 
+  @ApiProperty({
+    description: 'User password (minimum 6 characters)',
+    example: 'securePassword123',
+  })
   @IsString()
   @MinLength(6)
   password!: string;
--- core/src/user/interface/dtos/update-user.dto.ts
+++ core/src/user/interface/dtos/update-user.dto.ts
@@ -1,6 +1,11 @@
 import { IsEmail } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateUserDto {
+  @ApiProperty({
+    description: 'Updated user email address',
+    example: 'newemail@example.com',
+  })
   @IsEmail()
   email!: string;
 }
