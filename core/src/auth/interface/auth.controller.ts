53bbc1bc91843864c992cb8104cbdebd5cd9034e
Oleh Strokan <oleg14ua71@gmail.com>
2025-01-23 15:00:15 +0100

    add swagger decorators to auth controller

--- core/src/auth/interface/auth.controller.ts
+++ core/src/auth/interface/auth.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Inject, Post, Body } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiBody, ApiResponse } from '@nestjs/swagger'; // Import Swagger decorators
 import { ILoginUseCase } from '../epplication/use-cases/login/login.interface';
 import { LoginUseCase } from '../epplication/use-cases/login/login.use-case';
 import { IRegisterUseCase } from '../epplication/use-cases/register/register.interface';
 import { RegisterUseCase } from '../epplication/use-cases/register/register.use-case';
-import { LoginDto } from './dtos/login.dto';
+import { LoginRequestDto } from './dtos/login-request.dto';
 import { CreateUserDto } from 'src/user/interface/dtos/create-user.dto';
+import { LoginResponseDto } from './dtos/login-response.dto';
 
+@ApiTags('Authentication')
 @Controller('auth')
 export class AuthController {
   constructor(
@@ -17,12 +20,34 @@ export class AuthController {
   ) {}
 
   @Post('register')
+  @ApiOperation({ summary: 'Register a new user' })
+  @ApiBody({ type: CreateUserDto, description: 'User registration data' })
+  @ApiResponse({
+    status: 201,
+    description: 'User successfully registered',
+    type: String,
+  })
+  @ApiResponse({
+    status: 409,
+    description: 'User with this email already exits',
+  })
   async register(@Body() registerDto: CreateUserDto) {
     return await this.registerUseCase.execute(registerDto);
   }
 
   @Post('login')
-  async login(@Body() loginDto: LoginDto) {
+  @ApiOperation({ summary: 'Login a user' })
+  @ApiBody({ type: LoginRequestDto, description: 'User login data' })
+  @ApiResponse({
+    status: 200,
+    description: 'User successfully logged',
+    type: LoginResponseDto,
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Invalid email or password',
+  })
+  async login(@Body() loginDto: LoginRequestDto) {
     return await this.loginUseCase.execute(loginDto);
   }
 }
