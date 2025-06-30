4b02c8a7ab9a421a43a0df7f24b3b154d0421970
Oleh Strokan <oleg14ua71@gmail.com>
2025-06-30 13:15:16 +0200

    update product repository

--- core/src/product/core/product/repository/product.repository.ts
+++ core/src/product/core/product/repository/product.repository.ts
@@ -2,12 +2,16 @@ import { IClearableRepository } from 'src/shared/types/clearable';
 import { Product } from '../entity/product';
 import { ProductStatus } from '../entity/product-status';
 import { ProductData } from '../entity/product.interface';
+import { PaginatedResult } from 'src/shared/types/paginated-result';
 
 export abstract class IProductRepository extends IClearableRepository {
   abstract save(product: Product): Promise<Product>;
   abstract findById(id: string): Promise<Product | null>;
   abstract findBySku(sku: string): Promise<Product | null>;
-  abstract findAll(page: number, limit: number): Promise<Product[]>;
+  abstract findAll(
+    after?: string,
+    limit?: number,
+  ): Promise<PaginatedResult<Product>>;
   abstract deleteById(id: string): Promise<void>;
   abstract findByStatus(
     status: ProductStatus,
--- core/src/product/epplication/use-cases/find-products/find-product.interface.ts
+++ core/src/product/epplication/use-cases/find-products/find-product.interface.ts
@@ -1,5 +1,9 @@
 import { Product } from 'src/product/core/product/entity/product';
+import { PaginatedResult } from 'src/shared/types/paginated-result';
+import { FindProductsRequestDto } from './find-products.use-case';
 
 export abstract class IFindProductsUseCase {
-  abstract execute(): Promise<Product[]>;
+  abstract execute(
+    dto: FindProductsRequestDto,
+  ): Promise<PaginatedResult<Product>>;
 }
