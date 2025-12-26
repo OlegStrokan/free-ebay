0e1cced2e318c96ec3958a3cb8265e4ac01b9bc0
Oleh Strokan <oleg14ua71@gmail.com>
2025-12-26 13:23:28 +0100

    order-service: partial init

---
+++ Order/src/Domain/Common/AggregateRoot.cs
@@ -0,0 +1,22 @@
+namespace Domain.Common;
+
+public abstract class AggregateRoot : Entity
+{
+    private readonly List<IDomainEvent> _domainEvents = new();
+    
+    public IReadOnlyList<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+    
+    protected AggregateRoot(string id ) : base(id) {}
+    
+    protected AggregateRoot() : base() {}
+
+    protected void AddDomainEvent(IDomainEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
+    }
+
+    public void ClearDomainEvents()
+    {
+        _domainEvents.Clear();
+    }
+} 
