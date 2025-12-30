0eb3db3c7652a92726a63c89e36e13ff8d22045a
Oleh Strokan <oleg14ua71@gmail.com>
2025-12-30 14:12:11 +0100

    order-service: add base entity

---
+++ Order/src/Domain/Common/Entity.cs
@@ -0,0 +1,49 @@
+namespace Domain.Common;
+
+public abstract class Entity<TId>
+{
+    public TId Id { get; protected set; }
+
+
+    protected Entity(TId id)
+    {
+        Id = id;
+    }
+    
+    protected Entity() {}
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is not Entity<TId> other) 
+            return false;
+        
+        if (ReferenceEquals(this, other))
+            return true;
+
+        if (GetType() != other.GetType())
+            return false;
+
+        return Id?.Equals(other.Id) ?? false;
+    }
+
+    public override int GetHashCode()
+    {
+        return Id?.GetHashCode() ?? 0;
+    }
+
+    public static bool operator ==(Entity<TId>? a, Entity<TId>? b)
+    {
+        if (a is null && b is null)
+            return true;
+
+        if (a is null || b is null)
+            return false;
+
+        return a.Equals(b);
+    }
+
+    public static bool operator !=(Entity<TId>? a, Entity<TId>? b)
+    {
+        return !(a == b);
+    }
+}
