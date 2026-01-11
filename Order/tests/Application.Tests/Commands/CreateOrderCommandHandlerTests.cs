bb46da4878d6218eb9e60e46b20ab9243d8169e3
Oleh Strokan <oleg14ua71@gmail.com>
2026-01-11 08:14:15 +0100

    order-service: update create order command handler tests

--- Order/tests/Application.Tests/Commands/CreateOrderCommandHandlerTests.cs
+++ Order/tests/Application.Tests/Commands/CreateOrderCommandHandlerTests.cs
@@ -1,21 +1,33 @@
 using Application.Commands.CreateOrder;
 using Application.DTOs;
+using Application.Interfaces;
 using Domain.Entities;
+using Domain.Events;
 using Domain.Interfaces;
 using Microsoft.Extensions.Logging;
 using NSubstitute;
+using NSubstitute.ExceptionExtensions;
 
 namespace Application.Tests.Commands;
 
 public class CreateOrderCommandHandlerTests
 {
     private readonly IOrderRepository _orderRepository = Substitute.For<IOrderRepository>();
+    private readonly IOutboxRepository _outboxRepository = Substitute.For<IOutboxRepository>();
+    private readonly IUnitOfWork _unitOfWork = Substitute.For<IUnitOfWork>();
+    private readonly IDbContextTransaction _transaction = Substitute.For<IDbContextTransaction>();
     private readonly ILogger<CreateOrderCommandHandler> _logger = Substitute.For<ILogger<CreateOrderCommandHandler>>();
+
+    public CreateOrderCommandHandlerTests()
+    {
+        _unitOfWork.BeginTransactionAsync(Arg.Any<CancellationToken>())
+            .Returns(_transaction);
+    }
     
     [Fact]
     public async Task Handle_ShouldReturnSuccess_WhenOrderIsCreated()
     {
-        var handler = new CreateOrderCommandHandler(_orderRepository, _logger);
+        var handler = new CreateOrderCommandHandler(_orderRepository, _outboxRepository, _unitOfWork, _logger);
         var command = CreateValidCommand();
 
         var result = await handler.Handle(command, CancellationToken.None);
@@ -23,23 +35,34 @@ public class CreateOrderCommandHandlerTests
         Assert.True(result.IsSuccess);
 
         await _orderRepository.Received(1).SaveAsync(Arg.Any<Order>(), Arg.Any<CancellationToken>());
+        
+        await _outboxRepository.Received(1).AddAsync(
+            Arg.Any<Guid>(),
+            nameof(OrderCreatedEvent),
+            Arg.Any<string>(),
+            Arg.Any<DateTime>(),
+            Arg.Any<CancellationToken>());
+
+        await _transaction.Received(1).CommitAsync(Arg.Any<CancellationToken>());
     }
 
     [Fact]
     public async Task Handle_ShouldReturnFailure_WhenRepositoryThrows()
     {
-        var handler = new CreateOrderCommandHandler(_orderRepository, _logger);
+        var handler = new CreateOrderCommandHandler(_orderRepository, _outboxRepository, _unitOfWork, _logger);
         var command = CreateValidCommand();
 
-        // setup behavior
-        _orderRepository
-            .When(x => x.SaveAsync(Arg.Any<Order>(), Arg.Any<CancellationToken>()))
-            .Do(_ => throw new Exception("Db Error"));
+        _orderRepository.SaveAsync(Arg.Any<Order>(), Arg.Any<CancellationToken>())
+            .Throws(new Exception("Database crash"));
 
         var result = await handler.Handle(command, CancellationToken.None);
         
         Assert.False(result.IsSuccess);
-        Assert.Contains("Failed to create order", result.Error);
+
+        await _transaction.DidNotReceive().CommitAsync(Arg.Any<CancellationToken>());
+
+        await _transaction.Received(1).RollbackAsync(Arg.Any<CancellationToken>());
+
     }
 
     private CreateOrderCommand CreateValidCommand() => 
