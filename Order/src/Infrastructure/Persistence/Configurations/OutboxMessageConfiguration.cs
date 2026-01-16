362beb790b86fe2b505f14b58cd51dd80f959d81
Oleh Strokan <oleg14ua71@gmail.com>
2026-01-16 19:03:29 +0100

    order-service: add outbox persistence

---
+++ Order/src/Infrastructure/Persistence/Configurations/OutboxMessageConfiguration.cs
@@ -0,0 +1,29 @@
+using Application.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configurations;
+
+using Microsoft.EntityFrameworkCore;
+
+
+public class OutboxMessageConfiguration : IEntityTypeConfiguration<OutboxMessage>
+{
+    public void Configure(EntityTypeBuilder<OutboxMessage> builder)
+    {
+        builder.ToTable("OutboxMessages");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Type).IsRequired().HasMaxLength(255);
+        builder.Property(x => x.Content).IsRequired().HasColumnType("nvarchar(max)");
+        builder.Property(x => x.OccurredOnUtc).IsRequired();
+        builder.Property(x => x.ProcessedOnUtc);
+        // storage for stack traces of error details. @think: do we really need it?
+        builder.Property(x => x.Error).HasMaxLength(2000);
+
+        builder.HasIndex(x => x.ProcessedOnUtc).HasFilter("[ProcessedOnUtc] IS NULL");
+
+        builder.HasIndex(x => x.OccurredOnUtc);
+
+    }
+}
---
+++ Order/src/Infrastructure/Persistence/Repositories/OutboxRepository.cs
@@ -0,0 +1,54 @@
+using Application.Interfaces;
+using Application.Models;
+using Infrastructure.Persistence.DbContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence.Repositories;
+
+public class OutboxRepository(AppDbContext dbContext) : IOutboxRepository
+{
+    public async Task AddAsync(Guid messageId, string type, string content, DateTime occurredOn, CancellationToken ct)
+    {
+        var outboxMessage = new OutboxMessage(messageId, type, content, occurredOn);
+
+        await dbContext.OutboxMessages.AddAsync(outboxMessage, ct);
+        
+        // saveChanges called by unit of work (we use transaction in handler)
+        
+    }
+
+    public async Task<IEnumerable<OutboxMessage>> GetUnprocessedMessagesAsync(int batchSize, CancellationToken ct)
+    {
+        return await dbContext.OutboxMessages
+            .Where(m => m.ProcessedOnUtc == null)
+            .OrderBy(m => m.OccurredOnUtc)
+            .Take(batchSize)
+            .ToListAsync(ct);
+    }
+
+    public async Task MarkAsProcessedAsync(Guid messageId, CancellationToken ct)
+    {
+        var message = await dbContext.OutboxMessages
+            .FirstOrDefaultAsync(m => m.Id == messageId, ct);
+
+        if (message != null)
+        {
+            message.MarkAsProcessed(DateTime.UtcNow);
+            await dbContext.SaveChangesAsync(ct);
+        }
+        
+    }
+
+    public async Task DeleteProcessedMessagesAsync(DateTime olderThen, CancellationToken ct)
+    {
+        var oldMessages = await dbContext.OutboxMessages
+            .Where(m => m.ProcessedOnUtc != null && m.ProcessedOnUtc < olderThen)
+            .ToListAsync(ct);
+
+        if (oldMessages.Any())
+        {
+            dbContext.OutboxMessages.RemoveRange(oldMessages);
+            await dbContext.SaveChangesAsync(ct);
+        }
+    }
+}
